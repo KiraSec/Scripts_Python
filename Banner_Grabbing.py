227c271a1b92176397d657f96dd3450680f82692
KiraSec <80753682+KiraSec@users.noreply.github.com>
2021-07-18 22:48:38 -0300

    Delete Banner_Grabbing.py

--- Banner_Grabbing.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import socket
-alvo = input("ALVO: ")
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect_ex((alvo, 22))
-print(sock.recv(2048))
-sock.close()
